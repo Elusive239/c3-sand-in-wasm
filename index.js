634fdcc43c24a5e837990ca61ea2b21e9f34eb92
Elusive239 <56003608+Elusive239@users.noreply.github.com>
2025-02-01 19:49:36 -0500

    realized this method of image loading wont work (because C3 has no concept of async, and it uses async js code)

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function make_wasm(wasm_stream) {
         update: wasm_stream.instance.exports.wasm_update,
         get_flat_buffer: wasm_stream.instance.exports.get_flat_buffer,
         alloc_array: wasm_stream.instance.exports.wasm_alloc_array,
-        new_image: wasm_stream.instance.exports.new_image,
+        // new_image: wasm_stream.instance.exports.new_image,
     };
 }
 
@@ -39,10 +39,10 @@ async function instantiateWasmClient(url) {
             rand: () => {
                 return Math.random();
             },
-            load_image: (buffer, buffer_len) => {
-                const src = new TextDecoder().decode(new Uint8ClampedArray(wasm.memory.buffer, buffer, buffer_len));
-                return load_wasm_image(src);
-            }
+            // load_image: (buffer, buffer_len) => {
+            //     const src = new TextDecoder().decode(new Uint8ClampedArray(wasm.memory.buffer, buffer, buffer_len));
+            //     return load_wasm_image(src);
+            // }
          },
     };      
     const wasm_stream = await WebAssembly.instantiateStreaming(fetch(url), importObject);
@@ -69,34 +69,34 @@ async function render_buffer(){
     ctx.drawImage(bitmap, 0, 0);
 }
 
-/** @param {string} src*/
-async function load_wasm_image(src){
-    let ptr = 0;
-    const img = new Image();
-    img.src = src;
-    await img.decode().then( () => {
-            console.log("%s: %s", src, img);
-            //get image data (Pixel*)
-            const width = img.width;
-            const height = img.height;
-
-            const tcanvas = document.createElement('canvas');
-            const tctx    = canvas.getContext('2d');
-            tctx.drawImage( img, 0, 0 );
-            const rgba = tctx.getImageData(0,0,width,height).data;
-
-            const elems = width*height*4;
-            const data = wasm.alloc_array(8, elems);
-            const darray = new Uint8ClampedArray(wasm.memory.buffer, data, elems);
-            darray.set(rgba);
-            ptr = wasm.new_image(data, width, height);
-            console.log(ptr);
-        } 
-    ).catch((e) => {
-        console.log("Couldn't load img %s:\n%s", src, e);
-    });
-    return ptr;
-}
+// /** @param {string} src*/
+// function load_wasm_image(src){
+//     const img = new Image();
+//     img.decoding = "sync";
+//     img.src = src;
+//     img.decode() //.then( () => {console.log("%s: %s", src, img);} )
+//     .catch((e) => {
+//         console.log("Couldn't load img %s:\n%s", src, e);
+//     });
+//     // while(!img.complete);
+//     const width = img.width;
+//     const height = img.height;
+//     const tcanvas = document.createElement('canvas');
+//     tcanvas.width = width;
+//     tcanvas.height = height;
+//     const tctx    = tcanvas.getContext('2d');
+//     tctx.drawImage( img, 0, 0 );
+//     if(!(width == 0 || height ==0)){
+//         console.log("Image loaded..");
+//         const rgba = tctx.getImageData(0,0,width,height).data;
+//         const elems = width*height*4;
+//         const data = wasm.alloc_array(8, elems);
+//         const darray = new Uint8ClampedArray(wasm.memory.buffer, data, elems);
+//         darray.set(rgba);
+//         const ptr = wasm.new_image(data, width, height);
+//         return ptr;
+//     }return 0;
+// }
 
 function unload(){
     if(wasm === null) return;
