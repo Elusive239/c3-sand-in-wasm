38e0b5265d932b058e8434e908dee0c35d31de31
Elusive239 <56003608+Elusive239@users.noreply.github.com>
2025-02-12 21:30:33 -0500

    decided to refactor a bunch of C3 code to be nicer. also decided to refactor the JS and am running into an issue with trying to use requestAnimationFrame over setInterval...

--- index.js
+++ index.js
@@ -6,75 +6,62 @@ if(canvas === null){
 }
 const ctx = canvas.getContext('2d');
 
-//ADD FUNCTIONS HERE TO CALL C3 FUNCTIONS IN JS
-/** @param {WebAssembly.WebAssemblyInstantiatedSource} wasm_stream*/
-async function make_wasm(wasm_stream) {
-    return {
-        stream: wasm_stream,
-        memory: wasm_stream.instance.exports.memory ,
-        initialize: wasm_stream.instance.exports._initialize,
-        deinit: wasm_stream.instance.exports.wasm_deinit,
-        update: wasm_stream.instance.exports.wasm_update,
-        get_flat_buffer: wasm_stream.instance.exports.get_flat_buffer,
-        alloc_array: wasm_stream.instance.exports.wasm_alloc_array,
-        set_mouse_pos: wasm_stream.instance.exports.set_mouse_pos,
-        set_mouse_button_up: wasm_stream.instance.exports.set_mouse_button_up,
-        set_mouse_button_down: wasm_stream.instance.exports.set_mouse_button_down,
-    };
-}
-
 /** @param {string} url*/
-async function instantiateWasmClient(url) {
-    if(canvas === null){
-        return;
-    }
-
+async function startWasmClient(url) {
     canvas.height = 180;
     canvas.width = 320;
 
+    //ADD FUNCTIONS HERE TO CALL JS FUNCTIONS IN C3
     const importObject = {
         env: { 
-            //ADD FUNCTIONS HERE TO CALL JS FUNCTIONS IN C3
-            console_log: (buffer, buffer_len) => {
-                console.log(new TextDecoder().decode(new Uint8ClampedArray(wasm.memory.buffer, buffer, buffer_len)));
-            },
-            rand: () => {
-                return Math.random();
-            },
-            sqrt: (num) => {
-                return Math.sqrt(num);
-            },
-            set_js_dimensions: (width, height) =>{
-                canvas.height = height;
-                canvas.width = width;
+            console_log: (buffer, buffer_len) => console.log(new TextDecoder().decode(new Uint8ClampedArray(wasm.memory.buffer, buffer, buffer_len))),
+            rand: () => Math.random(),
+            sqrt: (num) => Math.sqrt(num),
+            set_js_dimensions: (width, height) =>{[canvas.height,canvas.width]  = [height, width];},
+            clear_canvas: ()=> { ctx.clearRect(0, 0, canvas.width, canvas.height); },
+            render: (ptr, width, height) => {
+                const imageData = new ImageData(new Uint8ClampedArray(wasm.memory.buffer, ptr, width*height*4), width, height);
+                createImageBitmap(imageData).then(
+                    (bitmap)=>{
+                        ctx.drawImage(bitmap, 0, 0);
+                    }
+                );
             },
          },
     };      
-    const wasm_stream = await WebAssembly.instantiateStreaming(fetch(url), importObject);
-    wasm = await make_wasm(wasm_stream);
-    wasm.initialize();
+    //ADD FUNCTIONS HERE TO CALL C3 FUNCTIONS IN JS
+    wasm = await WebAssembly.instantiateStreaming(fetch(url), importObject).then(
+        (wasm_stream) => {
+            return {
+                stream: wasm_stream,
+                memory: wasm_stream.instance.exports.memory ,
+                ...wasm_stream.instance.exports,
+            };
+        }
+    );
+    wasm._initialize();
     ctx.mozImageSmoothingEnabled = false;
     ctx.webkitImageSmoothingEnabled = false;
     ctx.msImageSmoothingEnabled = false;
     ctx.imageSmoothingEnabled = false;
     
-    setInterval(() => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        render_buffer();
-        wasm.update();
-    }, 0.01);
+    // setInterval(() => {
+    //     wasm.app_update();
+    // }, 0.01);
     // canvas.onmousemove = handleMouseMove(e);
-    addEventListener("mousemove", (event) => {getMousePos(event);});
+    window.requestAnimationFrame(js_app_update);
+    addEventListener("mousemove", (event) => {pollMousePos(event);});
     addEventListener("mouseup", (event) => {wasm.set_mouse_button_up(event.buttons);});
     addEventListener("mousedown", (event) => {wasm.set_mouse_button_down(event.buttons);});
 }
 
-async function render_buffer(){
-    const width = canvas.width;
-    const height = canvas.height;
-    const imageData = new ImageData(new Uint8ClampedArray(wasm.memory.buffer, wasm.get_flat_buffer(), width*height*4), width, height);
-    const bitmap = await createImageBitmap(imageData);
-    ctx.drawImage(bitmap, 0, 0);
+let lastTime = 0;
+
+function js_app_update(time){
+    const delta = time - lastTime;
+    wasm.app_update(delta);
+    lastTime = time;
+    window.requestAnimationFrame(js_app_update);
 }
 
 function unload(){
@@ -82,13 +69,13 @@ function unload(){
     wasm.deinit();
 }
 
-function getMousePos(evt) {
+function pollMousePos(evt) {
     const rect = canvas.getBoundingClientRect();
     const x = (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
     const y = ((evt.pageY - rect.top) / (rect.bottom - rect.top) * canvas.height) - window.scrollY;
     wasm.set_mouse_pos(x, y);
 }
 
-window.onload = instantiateWasmClient("./build/wasm/out.wasm");
+window.onload = startWasmClient("./build/wasm/out.wasm");
 window.onbeforeunload = unload();
 window.onunload = unload();
